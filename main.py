c638089b6f3c52ccd84af0320266a1efc0564b12
Matheus De Lima Freire <matheus.delimafreire@outlook.com>
2026-03-30 01:24:29 +0100

    improved the ChatBot to persist state using the Conversations API with Responses API instead of using simple lists

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import typer
 from openai import OpenAI
+from openai.types.conversations.conversation import Conversation
 
 app = typer.Typer()
 client = OpenAI()
 
-def chat(user_input: str, history: list[dict]):
+def chat(user_input: str, history: list[dict]) -> str:
 
     history.append({"role": "user", "content": user_input})
 
@@ -19,12 +20,26 @@ def chat(user_input: str, history: list[dict]):
     return response.output_text
 
 
+def chat_with_conversations(user_input: str, conversation: Conversation, system_prompt: str) -> str:
+
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=[{"role": "developer", "content": system_prompt}, 
+               {"role": "user", "content": user_input}],
+        conversation=conversation.id
+    )
+
+    return response.output_text
+
+
+
 @app.command()
 def start_chat(system_prompt: str = "You are a helpful assistant."):
 
     typer.echo("Chat started. Type 'exit' to quit.\n")
 
     history = [{"role": "developer", "content": system_prompt}]
+    conversation: Conversation = client.conversations.create()
 
     while True:
         user_input: str = typer.prompt("You")
@@ -33,7 +48,7 @@ def start_chat(system_prompt: str = "You are a helpful assistant."):
             typer.echo("Goodbye!")
             raise typer.Exit()
         
-        response = chat(user_input, history)
+        response = chat_with_conversations(user_input, conversation, system_prompt)
         typer.echo(f"Assistant: {response}\n")
 
 
